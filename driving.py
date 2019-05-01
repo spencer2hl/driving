4adabd7dc61174b5baf31f07550c476cc7e6e0a8
spencer2hl <frost0901@outlook.com>
2019-05-01 02:35:58 -0500

    add America and else

--- driving.py
+++ driving.py
@@ -1,8 +1,15 @@
 country = input('请问你是哪国人：')
 age = input('请输入你的年龄：')
 age = int(age)
-if country == '台湾':
+if country == '中国':
 	if age >= 18:
 		print('你可以考驾照')
 	else:
 		print('你还不能考驾照')
+elif country == '美国':
+	if age >= 16:
+		print('你可以考驾照')
+	else:
+		print('你还不能考驾照')
+else:
+	print('请根据各国条例执行')
